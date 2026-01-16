f08f78e582a05956b00859280bdb073129b512df
Kuljira Namjai <synthiawithsyren@gmail.com>
2026-01-16 10:45:06 +0700

    practical/p05

--- src/practical/p05/index.ts
+++ src/practical/p05/index.ts
@@ -1,2 +1,28 @@
-export function safeFetchComment() {}
+import axios from "axios";
+
+
+interface Comment {
+  commentId: number;
+  id: number;
+  name?: string;
+  email: string;
+  body: string;
+}
+
+interface FilterComment {
+  id: number;
+  body: string;
+}
+
+export async function safeFetchComment(): Promise<FilterComment[] | null> {
+  const res = await axios.get<Comment[]>('https://jsonplaceholder.typicode.com/comments/{id}');
+  const comments = res.data;
+  try {
+    const { id, body } = comment;
+
+    return { id, body };
+  } catch (error) {
+    return null;
+  }
+}
 
