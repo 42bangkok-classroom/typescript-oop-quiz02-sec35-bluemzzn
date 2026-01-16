d5d8851154479e20f93789ace9cf8f2e2ed5101a
Kuljira Namjai <synthiawithsyren@gmail.com>
2026-01-16 09:41:25 +0700

    practical/p01

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,36 @@
-export function getEdgePosts() {}
+import axios from 'axios';
+import { title } from 'node:process';
+
+interface Post {
+  userId?: number;
+  id: number;
+  title: string;
+  body?: string;
+}
+
+interface EdgePost {
+  id: number;
+  title: string;
+}
+
+export async function getEdgePosts(): Promise<EdgePost[]> {
+  try {
+    const res = await axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts");
+    const posts = res.data;
+
+    if (posts.length === 0) {
+      return [];
+    }
+
+    const firstPost = posts[0];
+    const lastPost = posts[posts.length - 1];
+
+    return [
+      { id: firstPost.id, title: firstPost.title },
+      { id: lastPost.id, title: lastPost.title }
+    ].map(post => ({ id: post.id, title: post.title }))
+
+  } catch (error) {
+    throw new Error(`Failed to fetch posts: ${error instanceof Error ? error.message : 'Unknown error'}`);
+  }
+}
