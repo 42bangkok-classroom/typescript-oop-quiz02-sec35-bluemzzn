9cb625eced44e157fdcc3ef1b8473c6ad8a47d8f
Kuljira Namjai <synthiawithsyren@gmail.com>
2026-01-16 10:01:50 +0700

    practical/p02

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -1,12 +1,29 @@
+import axios from "axios";
 
 
-export async function getPostsByUser() {
+
+interface Post {
+  userId?: number;
+  id: number;
+  title: string;
+  body?: string;
+}
+
+interface filterPost {
+  id: number;
+  title: string;
+}
+
+export async function getPostsByUser(userId : number): Promise<filterPost[]> {
   try {
     const response = await axios.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
     const posts = response.data;
 
     return posts
-     
+    .filter(post => post.userId === userId)
+    .map(post => ({ id: post.id, title: post.title }));
+
   } catch (error) {
     throw new Error(`Failed to fetch posts: ${error instanceof Error ? error.message : 'Unknown error'}`);
   }
+}
